7572f21611cd7faab77f149e9c6f89b0dc2d5d9b
Solidus Snake <linkmerili@gmail.com>
2018-12-02 18:47:51 +0200

    A simple sentence program

---
+++ Sentences.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    age = input("Your age?") 
+    proffesion = input("Your proffesion?")
+    activity = input("What are you doing at the moment?")
+    print("Hello, I am " + age + " old and I am " + proffesion + " and I am doing " + activity)
